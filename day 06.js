a8f188d6b96ff6e479f56e42b2f3fbcacfdd0ebc
Perkine <Perkine@users.noreply.github.com>
2017-12-08 15:05:14 +0100

    Update day 06

--- day 06.js
+++ day 06.js
@@ -1,23 +1,24 @@
-
 var data = '4	10	4	1	8	4	9	14	5	1	14	15	0	15	3	5'.split('\t').map(Number);
 
 var cache = [data.join('|')];
 
-var c = 0;
-
 outer:while(true) {
- c++;
  var idx = data.indexOf(Math.max(...data));
  var val = data[idx];
  data[idx] = 0;
+
  for(var i=idx+1; i<idx+val+1; i++){
   data[i % data.length]++;
  }
 
- if(~cache.indexOf(data.join('|')) && cache.indexOf(data.join('|')) != cache.lastIndexOf(data.join('|'))){
+ var joined = data.join('|');
+ var idx = cache.indexOf(joined);
+
+ if(~idx){
   break outer;
  }
- cache.push(data.join('|'));
+
+ cache.push(joined);
 }
 
-console.log(cache.lastIndexOf(data.join('|')), c - cache.lastIndexOf(data.join('|')));
+console.log(cache.length,  cache.length - cache.indexOf(data.join('|')));
